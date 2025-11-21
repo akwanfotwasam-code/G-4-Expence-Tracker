ceb66ad825d5de2c6362f1850bcf3adb580add2f
Alex <alex.csec040@gmail.com>
2025-11-21 12:00:59 +0000

    Add files via upload

---
+++ tracking.js
@@ -0,0 +1,41 @@
+
+// Add Expenses Name and Anount
+// Validate Amount as Number
+// Display items in a table
+// totoal using loop
+// Allow delete of Any Item on Table
+
+deleTebleRowsCollected(){
+
+    const table = document.getElementById(`myTable`);
+    const rowsTable = [];
+
+    for (let i = 0; i < table.rows.lenght; i++){
+        const row = table.rows[i];
+        if (rows.cells[0].textContemt === `Remove This`){
+            rowsToDelete.push(row);
+
+        }
+    }
+   
+    for (let i = 0; i< rowsToDelete.length.i++)
+        {
+        rowsToDelete[i],remove(); 
+    }
+        const row = table.rows[i]; 
+        if (rows.cells[1].textConntent === `Remove This`){
+        rowsToDelete.push(row)
+        
+        
+        }
+
+
+
+
+
+
+    
+
+    }
+    
+// Use Functiosn and DDOM Mnipulations
